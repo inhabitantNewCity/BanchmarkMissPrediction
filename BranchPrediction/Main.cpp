3caa00dc580957efda0ab8941d3839cc9cc2d29d
inhabitantNewCity <v.andreev.job@gmail.com>
2018-12-08 19:50:52 +0300

    move to one banchmark

--- BranchPrediction/Main.cpp
+++ BranchPrediction/Main.cpp
@@ -2,7 +2,7 @@
 #include <ctime>
 #include <iostream>
 
-double do_branch(bool sorting, bool cond_assign) {
+double do_branch(bool sorting) {
 	const unsigned arraySize = 32768;
 	int data[arraySize];
 
@@ -16,17 +16,13 @@ double do_branch(bool sorting, bool cond_assign) {
 	clock_t start = clock();
 	long long sum = 0;
 
-	for (unsigned i = 0; i < 10000; ++i)
+	for (unsigned i = 0; i < 100000; ++i)
 	{
 		for (unsigned c = 0; c < arraySize; ++c)
 		{
-			if (cond_assign) {
-				sum += data[c] >= 128 ? data[c] : 0;
-			}
-			else {
+
 				if (data[c] >= 128)
 					sum += data[c];
-			}
 		}
 	}
 
@@ -37,17 +33,11 @@ int main()
 {	
 	std::cout << " Banchmark for calculating sum of elements" << std::endl;
 
-	double NonSortAndNonCond = do_branch(false, false);
-	std::cout << " time without Sort And Cond " << NonSortAndNonCond << std::endl;
+	double NonSortAndNonCond = do_branch(false);
+	std::cout << " time without Sort: " << NonSortAndNonCond << std::endl;
 	
-	double SortAndNonCond = do_branch(true, false);
-	std::cout << " time with Sort but without Cond " << SortAndNonCond << std::endl;
-
-	double NonSortAndCond = do_branch(false, true);
-	std::cout << " time without Sort but with Cond " << NonSortAndCond << std::endl;
-
-	double SortAndCond = do_branch(true, true);
-	std::cout << " time with Sort And Cond " << SortAndCond << std::endl;
+	double SortAndNonCond = do_branch(true);
+	std::cout << " time with Sort: " << SortAndNonCond << std::endl;
 
 	getchar();
 }
